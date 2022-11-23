43020f36002fdfcd927ac86e907921cb1ef3e901
andkleven <amkleven@gmail.com>
2022-11-23 14:40:39 +0100

    token

--- token/getToken.ts
+++ token/getToken.ts
@@ -4,13 +4,15 @@ export const getToken = () => {
   let token: TokenDecoded | null;
   let key = `oidc.user:${process.env.NEXT_PUBLIC_AUTHORITY}/:catacloud-web`;
   console.log("key", key);
-  token = JSON.parse(String(window.localStorage.getItem(key)));
-  console.log("token", token);
-  if (token) {
-    return token;
-  } else {
-    key = `oidc.user:${process.env.NEXT_PUBLIC_AUTHORITY}:${process.env.NEXT_PUBLIC_CLIENT_ID}`;
-    token = JSON.parse(String(window.sessionStorage.getItem(key)));
-    return token;
+  const catacloudWindow = window.open("https://app.catacloud.com/");
+  if (catacloudWindow?.localStorage.getItem(key)) {
+    token = JSON.parse(String(catacloudWindow?.localStorage.getItem(key)));
+    if (token) {
+      return token;
+    }
+    console.log("token", token);
   }
+  key = `oidc.user:${process.env.NEXT_PUBLIC_AUTHORITY}:${process.env.NEXT_PUBLIC_CLIENT_ID}`;
+  token = JSON.parse(String(window.sessionStorage.getItem(key)));
+  return token;
 };
