94c1dc13748394a21e3d0cb6e21ea883ff26deb9
andkleven <amkleven@gmail.com>
2022-11-23 14:30:37 +0100

    token

--- token/getToken.ts
+++ token/getToken.ts
@@ -3,7 +3,9 @@ import { TokenDecoded } from "./types";
 export const getToken = () => {
   let token: TokenDecoded | null;
   let key = `oidc.user:${process.env.NEXT_PUBLIC_AUTHORITY}:catacloud-web`;
+  console.log("key", key);
   token = JSON.parse(String(window.localStorage.getItem(key)));
+  console.log("token", token);
   if (token) {
     return token;
   } else {
