e722f4c09e0c4f9f38dc2e42adbf9415035eca60
andkleven <amkleven@gmail.com>
2022-11-23 14:32:56 +0100

    get token

--- token/getToken.ts
+++ token/getToken.ts
@@ -2,7 +2,7 @@ import { TokenDecoded } from "./types";
 
 export const getToken = () => {
   let token: TokenDecoded | null;
-  let key = `oidc.user:${process.env.NEXT_PUBLIC_AUTHORITY}:catacloud-web`;
+  let key = `oidc.user:${process.env.NEXT_PUBLIC_AUTHORITY}/:catacloud-web`;
   console.log("key", key);
   token = JSON.parse(String(window.localStorage.getItem(key)));
   console.log("token", token);
